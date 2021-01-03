054178c4b8bed1d25466b17b8ba0b40673ff1c7e
AISHWARYA <aishwarya25252@gmail>
2021-01-03 19:35:03 +0530

    _

--- underDevelopment/_preprocessing.py
+++ underDevelopment/_preprocessing.py
@@ -1,1 +1,15 @@
 #implementation of certain useful functions
+import numpy as np
+import pandas as pd
+
+def random_normalised_centroids(shape):
+
+    return np.random.rand(shape[0],shape[1])
+
+def normalise_dataframe(dataframe):
+    
+    return (dataframe.T/dataframe.max(0).values.reshape(dataframe.shape[1],1)).T
+
+def import_dataframe(name,delimiter):
+
+    return pd.read_csv(name,sep=delimiter,index_col=0).T
